493d3acd1da1d1477da70688156b758cd123080a
diadatp <admin@diadatp.com>
2021-09-21 12:44:27 +0530

    Added script to include nvidia drivers in docker images.

---
+++ tools/add_nvidia_drivers.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# based on https://github.com/tianon/docker-bin/blob/master/nvidiize
+
+# gameonwhales/xorg
+# gameonwhales/steam
+
+DOCKER_BUILDKIT=1 docker build - --tag $1 --build-arg FROM=$1 << 'EOF'
+ARG FROM
+FROM ${FROM}
+
+ARG NVIDIA_DRIVER_VERSION=465.19.01
+
+RUN dpkg --add-architecture i386 && \
+    apt-get update && \
+    apt-get upgrade --yes && \
+    apt-get install --no-install-recommends --yes \
+        libegl1 \
+        libegl1:i386 \
+        libgl1 \
+        libgl1:i386 \
+        libgles2 \
+        libgles2:i386 \
+        libglvnd0 \
+        libglvnd0:i386 \
+        libglx0 \
+        libglx0:i386 \
+        libopengl0 \
+        libopengl0:i386 && \
+    apt-get install --no-install-recommends --yes \
+        ca-certificates \
+        kmod \
+        pkg-config \
+        wget && \
+    wget --quiet http://us.download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_DRIVER_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_DRIVER_VERSION}.run && \
+    sh NVIDIA-Linux-x86_64-${NVIDIA_DRIVER_VERSION}.run \
+        --install-compat32-libs \
+        --install-libglvnd \
+        --no-backup \
+        --no-check-for-alternate-installs \
+        --no-kernel-module \
+        --no-kernel-module-source \
+        --no-nouveau-check \
+        --no-nvidia-modprobe \
+        --no-questions \
+        --ui=none && \
+    rm NVIDIA-Linux-x86_64-${NVIDIA_DRIVER_VERSION}.run && \
+    rm -rf /var/lib/apt/lists/*
+EOF
