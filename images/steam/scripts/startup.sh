5d5a1bf908ad5a68d5c29344a43aa888a42b03f1
diadatp <admin@diadatp.com>
2021-09-28 21:38:41 +0530

    Enabling wget progress update and modifying required groups to support Vulkan.

--- images/steam/scripts/startup.sh
+++ images/steam/scripts/startup.sh
@@ -10,10 +10,10 @@ wait-x11
 
 LOG "Starting Steam with DISPLAY=${DISPLAY}"
 
-ensure-groups ${GOW_REQUIRED_DEVICES:-/dev/uinput /dev/input/event*}
+ensure-groups ${GOW_REQUIRED_DEVICES:-/dev/uinput /dev/input/event* /dev/dri/* /dev/snd/*}
 
 # Recursively creating Steam necessary folders (https://github.com/ValveSoftware/steam-for-linux/issues/6492)
 mkdir -p $HOME/.steam/ubuntu12_32/steam-runtime
 
 # Start Steam. Use `sudo` to make sure that group membership gets reloaded
-exec sudo -u $(whoami) -E /usr/games/steam
+exec sudo -u $(whoami) -E steam
